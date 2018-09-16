2e240fd21737bc867a0bf5d8dd6791b038f943c4
James Greig <3stripe@gmail.com>
2018-09-16 13:59:51 +0100

    Fix date format in log message

--- web/data/get-tweets.sh
+++ web/data/get-tweets.sh
@@ -3,4 +3,4 @@ curl "https://api.airtable.com/v0/app6c5PGL8ECac8DI/2018?maxRecords=999&view=Cle
 -H "Authorization: Bearer keyhIK1RE4i5B0qJ3" -o "/home/forge/happylager.ghdev.xyz/web/data/dotall-tweets.json"
 wait
 wget -q -O- "https://happylager.ghdev.xyz/index.php?p=actions/feed-me/feeds/run-task&direct=1&feedId=3&passkey=P4mvCU2XLR" > /dev/null
-echo "\n date +"%m-%d-%Y" - tweets imported :) \n"
+echo $(date +"+%H-%M_%m-%d-%Y") - Tweets imported
