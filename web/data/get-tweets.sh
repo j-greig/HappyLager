18bd53059c88cd8bb39c7a96be0aef42b5542991
James Greig <3stripe@gmail.com>
2018-09-16 11:48:35 +0100

    Shell script to get DotAll tweets from Airtable

---
+++ web/data/get-tweets.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl "https://api.airtable.com/v0/app6c5PGL8ECac8DI/2018?maxRecords=999&view=Grid%20view" \
+-H "Authorization: Bearer keyhIK1RE4i5B0qJ3" -o "/home/forge/happylager.ghdev.xyz/web/data/dotall-tweets.json"
+wait
+wget -q -O- "https://happylager.ghdev.xyz/index.php?p=actions/feed-me/feeds/run-task&direct=1&feedId=2&passkey=mwfDTaDPSL" > /dev/null
+echo "\n date +"%m-%d-%Y" - tweets imported :) \n"
